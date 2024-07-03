ae6829758ff2dde34fa598df5140dce2e35542bd
Mouloud Hasrane <145935984+mouloud240@users.noreply.github.com>
2024-07-03 23:51:56 +0100

    Update Tasks(delete after project ending).js

--- app/Tasks(delete after project ending).js
+++ app/Tasks(delete after project ending).js
@@ -1,9 +1,1 @@
- {/*
-       //navbar
-    hero(done almost)
-    //services
-    app download(done still need to add the download button icon and fix the spacing a bit) 
-    //feedback
-    article
-    footer(done)
-    */ }
+
